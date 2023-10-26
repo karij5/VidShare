419263619e68c78cc06e737c744754882d8a242b
karij5 <karij5@farmingdale.edu>
2023-10-26 15:56:50 -0400

    Sign in, Sign up FXML and controllers set up

---
+++ src/main/java/com/vidshare/vidshare/SignInController.java
@@ -0,0 +1,49 @@
+package com.vidshare.vidshare;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignInController {
+    @FXML
+    private Label emailText;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Label passwordText;
+    @FXML
+    private Button signInButton;
+    @FXML
+    private Button signUpButton;
+
+
+    @FXML
+    protected void onSignInButtonClick() {
+        //if(!emailField.getText().contains("@")){
+
+        //}
+    }
+
+
+    @FXML
+    public void onSignUpHyperlinkClick(Event event) throws IOException {
+        Node node=(Node) event.getSource();
+        Stage stage=(Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("signup-view.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/main/java/com/vidshare/vidshare/StartupApplication.java
+++ src/main/java/com/vidshare/vidshare/StartupApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class StartupApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartupApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartupApplication.class.getResource("signin-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("VidShare");
         stage.setResizable(false);
