0292453ab5da0e15b698275a48ad4843e29ac96d
David Mesquita <dmesquita5678@gmail.com>
2023-11-21 15:16:48 -0500

    Firebase

---
+++ src/main/java/com/vidshare/vidshare/FirestoreContext.java
@@ -0,0 +1,35 @@
+package com.vidshare.vidshare;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.auth.Credentials;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
+import com.google.firebase.cloud.FirestoreClient;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.google.auth.oauth2.GoogleCredentials.*;
+import static com.google.auth.oauth2.ServiceAccountCredentials.fromStream;
+
+public class FirestoreContext {
+
+    public Firestore firebase() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(fromStream(getClass().getResourceAsStream("key.json")))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return FirestoreClient.getFirestore();
+    }
+}
--- src/main/java/com/vidshare/vidshare/StartupApplication.java
+++ src/main/java/com/vidshare/vidshare/StartupApplication.java
@@ -1,5 +1,7 @@
 package com.vidshare.vidshare;
 
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.auth.FirebaseAuth;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,8 +10,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class StartupApplication extends Application {
+
+    public static Firestore fstore;
+    public static FirebaseAuth fauth;
+    private final FirestoreContext contxtFirebase = new FirestoreContext();
     @Override
     public void start(Stage stage) throws IOException {
+        fstore = contxtFirebase.firebase();
+        fauth = FirebaseAuth.getInstance();
+
         FXMLLoader fxmlLoader = new FXMLLoader(StartupApplication.class.getResource("signin-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("VidShare");
